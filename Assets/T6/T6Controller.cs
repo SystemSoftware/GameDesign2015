7e920d253a01c99950d0f604b7b610551589a35e
Alexander Landmesser <alexander@land-server.de>
2015-05-15 15:57:36 +0200

    Added T6 (A-Team) initial setup

---
+++ Assets/T6/T6Controller.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class T6Controller : Controller {
+
+    public bool cameraLock = true;
+
+	// Update is called once per frame
+	void Update () {
+        float acceleration = Input.GetAxis(ctrlAxisAccelerate)+1;
+        float horizontal = Input.GetAxis(ctrlAxisHorizontal);
+        float vertical = Input.GetAxis(ctrlAxisVertical);
+        float yaw = Input.GetAxis(ctrlAxisOther);
+        foreach (RotateThrustFlaps s in this.GetComponentsInChildren<RotateThrustFlaps>())
+        {
+            s.Rotate(acceleration, vertical, yaw);
+        }
+	}
+
+
+    //LateUpdate for smooth custom camera follow
+   void LateUpdate()
+    {
+        Transform target = this.transform;
+        Transform camera = ctrlAttachedCamera.transform;
+        Vector3 forward = target.forward * 400.0f;
+        Vector3 needPos = target.position - forward + (target.up * 50);
+
+        if (cameraLock) { 
+        camera.LookAt(target.transform);
+        camera.position = needPos;
+        camera.rotation = target.rotation;
+   }
+    }
+}
---
+++ Assets/T6/T6EngineDriver.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class T6EngineDriver : EngineDriver {
+
+    string pitch;
+    string yaw;
+
+	// Override EngineDriver Update function to scale the input without a backward range
+    void Update()
+    {
+
+
+        //bool enabled = Input.GetKey(key);
+
+        if (axis == null || axis.Length == 0)
+            axis = GetComponentInParent<T6Controller>().ctrlAxisAccelerate;
+        {
+            float f = 0f;
+            if (axis != null)
+            {
+                f = (Input.GetAxis(axis)+1)/2f;
+            }
+            if (sys != null)
+            {
+                sys.enableEmission = f != 0f;
+                sys.startLifetime = f * maxLifetime;
+            }
+
+            if (force != null)
+            {
+               force.relativeForce = new Vector3(0, 0, maxForce * f);
+            }
+        }
+
+
+    }
+}
