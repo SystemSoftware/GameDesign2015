90f4b13a655f83f05ccc00865d28ce83fee2be33
Stefan Elsen <stefan.elsen@hotmail.com>
2015-05-19 13:55:32 +0200

    Fixed center of gravity visualization

--- Assets/ShipSize.cs
+++ Assets/ShipSize.cs
@@ -6,7 +6,8 @@ public class ShipSize : MonoBehaviour {
 
 //    public bool setSize = false;
     public int triangleCount = 0;
-    
+    public bool showCentersOfGravity = true;
+
     public Vector3 currentSize;
     public Vector3 wantSize;
     public bool applySize = false;
@@ -79,16 +80,46 @@ public class ShipSize : MonoBehaviour {
 
 
 			//Draw center of mass
-			Rigidbody body = this.GetComponent<Rigidbody>();
-			if(body != null){
-				Debug.DrawLine(body.worldCenterOfMass,body.transform.right*10,Color.red);
-				Debug.DrawLine(body.worldCenterOfMass,body.transform.up*10,Color.green);
-				Debug.DrawLine(body.worldCenterOfMass,body.transform.forward*10,Color.blue);
-			}
+            if (showCentersOfGravity)
+            {
+                foreach (var body in this.GetComponentsInChildren<Rigidbody>())
+                {
+                    DrawEdge(body.worldCenterOfMass, new Vector3(-1, 0, 0), new Vector3(1, 0, 0));
+                    DrawEdge(body.worldCenterOfMass, new Vector3(0, -1, 0), new Vector3(0, 1, 0));
+                    DrawEdge(body.worldCenterOfMass, new Vector3(0, 0, -1), new Vector3(0, 0, 1));
+
+
+                    //Rigidbody body = this.GetComponent<Rigidbody>();
+             //       if (body != null)
+                    //{
+                    //    for (int x = 0; x < 2; x++)
+                    //        for (int y = x + 1; y < 3; y++)
+                    //        {
+                                
+                    //            for (int k = 0; k < 4; k++)
+                    //            {
+                    //                Vector3 v = new Vector3(0,0,0),
+                    //                        w = new Vector3(0,0,0);
+                    //                v[x] = (k%2)==0? (k/2==0 ? 1 : -1) : 0;
+                    //                v[y] = (k % 2) != 0 ? (k / 2 == 0 ? 1 : -1) : 0;
+                    //                w[x] = (k % 2) != 0 ? ((k == 0 || k == 3)? 1 : -1) : 0;
+                    //                w[y] = (k % 2) == 0 ? ((k == 0 || k == 3) ? 1 : -1) : 0;
+                    //                DrawEdge(body.worldCenterOfMass, v, w);
+                    //            }
+                    //        }
+
+                    //}
+                }
+            }
         }
         else
             this.enabled = false;
 
 
 	}
+
+    void DrawEdge(Vector3 c, Vector3 v, Vector3 w)
+    {
+        Debug.DrawLine(c + v * 10f, c + w * 10f, Color.red);
+    }
 }
