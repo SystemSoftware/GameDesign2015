d2b4c5e988be40359199b596f82ab5b233d35fc8
Alexander Landmesser <alexander@land-server.de>
2015-07-09 13:07:46 +0200

    Added 3rd axis stabilization, added first 4 gates to mark a racepath

---
+++ Assets/T6/T6PitchCorrection.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class T6PitchCorrection : MonoBehaviour
+{
+
+    T6Controller controller;
+    float yaw;
+    float lastForce;
+    Rigidbody ship;
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = this.GetComponentInParent<T6Controller>();
+        ship = this.transform.parent.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 localAngularVelocity = ship.transform.InverseTransformDirection(ship.angularVelocity);
+        yaw = Input.GetAxis(controller.ctrlAxisOther);
+        if (yaw == 0)
+        {
+            setForce(localAngularVelocity.y * -5000);
+        }
+        else
+        {
+            setForce(yaw * 2000);
+        }
+
+    }
+    void setForce(float f)
+    {
+        this.GetComponent<ConstantForce>().relativeForce = new Vector3(0, 0, f);
+        this.GetComponent<ParticleSystem>().startSpeed = f / -20;
+    }
+}
