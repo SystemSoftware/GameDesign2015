44d11804861695e3ff26e7af4a887919b96ca2ab
BobPr <gecko_tc@yahoo.de>
2015-07-14 12:55:15 +0200

    Enemyships Part1

---
+++ Assets/T4/EnemyShip/T4EnPath.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class T4EnPath : MonoBehaviour {
+    private GameObject[] path_objects;
+    private Vector3[] path_points;
+    private int object_count;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void collectPathObjects() {
+        Transform[] childs = transform.GetComponentsInChildren<Transform>();
+        path_objects = new GameObject[childs.Length];
+        int i = 0;
+
+        // collect all objects who form the path
+        foreach (Transform c in childs)
+        {
+            path_objects[i++] = c.gameObject;
+        }
+        object_count = i;
+
+        List<Vector3> path_points_list = new List<Vector3>();
+        // calc the points inbetween the path_objects
+        for (int k = 1; k < object_count - 1; k++)
+        {
+            Vector3 prev = path_objects[k - 1].transform.position;
+            Vector3 pos = path_objects[k].transform.position;
+
+            Vector3 toAdd = (pos - prev).normalized * 8;
+            // set starting point
+            Vector3 current_point_between_objects = prev;
+            while (Vector3.Distance(prev, pos) > Vector3.Distance(prev, current_point_between_objects))
+            {
+                path_points_list.Add(current_point_between_objects);
+                // next point
+                current_point_between_objects += toAdd;
+            }
+        }
+
+        path_points = new Vector3[path_points_list.Count];
+        // convert list to array
+        for (int j = 0; j < path_points_list.Count - 1; j++) { 
+            path_points[j] = path_points_list[j];
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color32(138, 239, 255, 255);
+        if (path_objects == null)
+        {
+            collectPathObjects();
+        }
+
+        for (int k = 2; k < path_objects.Length; k++)
+        {
+            Vector3 prev = path_objects[k - 1].transform.position;
+            Vector3 pos = path_objects[k].transform.position;
+
+            Gizmos.DrawLine(prev, pos);
+        }
+    }
+    
+}
---
+++ Assets/T4/EnemyShip/T4EnemyShips.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class T4EnemyShips : MonoBehaviour {
+    private GameObject[] enemies;
+
+	// Use this for initialization
+	void Start () {
+        collectAllShips();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void collectAllShips()
+    {
+        enemies = new GameObject[transform.childCount];
+
+        Transform[] childs = transform.GetComponentsInChildren<Transform>();
+        for(int i= 0; i<transform.childCount; i++){
+            Debug.Log("LAYER "+this.gameObject.layer+" enem collected > "+(i)+" > "+transform.GetChild(i));
+            
+        }
+    }
+}
