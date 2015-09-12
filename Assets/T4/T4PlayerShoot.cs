ccb5d6d4b3046d0f28ace7b4fd084e52dde70d14
BobPr <gecko_tc@yahoo.de>
2015-09-12 12:18:03 +0200

    Additional Sounds and Cave Upgrades

--- Assets/T4/T4PlayerShoot.cs
+++ Assets/T4/T4PlayerShoot.cs
@@ -63,25 +63,25 @@ public class T4PlayerShoot : MonoBehaviour {
 
 	//Fires Laser, tests if Target Hit, speeds up if target hit
 	IEnumerator Fire(){
-		
-		RaycastHit hit; //raycast to test for hit
-		var origin = transform.position; //origin of laser
-		var direction = transform.forward; //direction of laser
-		var endPoint = origin + direction * 400; //endpoint of Laser
-		int skipShips = ~((1<<8)|(1<<9)|(1<<10)|(1<<11)); //LayerMask to ignore Ship-Colliders with Raycast
-		if(Physics.Raycast (origin, direction, out hit, 400f, skipShips)){ //Raycast, ignores Ships
-			endPoint=hit.point; //Change endpoint of Laser to hitpoint
-			if(hit.transform.tag == "Target"){
-				Destroy (hit.transform.gameObject); //if hit.point belongst to gameobject of type target, destroy it
-				SpeedUp (); //speed boost for the ship
-			}
-		}
-		lineRenderer.SetVertexCount(2); //show the shot
-		lineRenderer.SetPosition(0, origin);
-		lineRenderer.SetPosition(1, endPoint);
-		StartCoroutine(DisableRenderer());//blend the shot out after some time
-		yield return new WaitForSeconds(1);//limit the firerate
-		allowfire = true;//allow the next shot to be fired
+            RaycastHit hit; //raycast to test for hit
+            var origin = transform.position; //origin of laser
+            var direction = transform.forward; //direction of laser
+            var endPoint = origin + direction * 400; //endpoint of Laser
+            int skipShips = ~((1 << 8) | (1 << 9) | (1 << 10) | (1 << 11)); //LayerMask to ignore Ship-Colliders with Raycast
+            if (Physics.Raycast(origin, direction, out hit, 400f, skipShips)) { //Raycast, ignores Ships
+                endPoint = hit.point; //Change endpoint of Laser to hitpoint
+                if (hit.transform.tag == "Target") {
+                    Destroy(hit.transform.gameObject); //if hit.point belongst to gameobject of type target, destroy it
+                    SpeedUp(); //speed boost for the ship
+                }
+            }
+            lineRenderer.SetVertexCount(2); //show the shot
+            lineRenderer.SetPosition(0, origin);
+            lineRenderer.SetPosition(1, endPoint);
+            StartCoroutine(DisableRenderer());//blend the shot out after some time
+            yield return new WaitForSeconds(1);//limit the firerate
+            allowfire = true;//allow the next shot to be fired
+        
 	}
 
 	void FixedUpdate(){
