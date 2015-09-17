28b24f730f469642dfb288f78a6944ecc95cc9b4
unknown <rmuellers>
2015-09-17 17:59:57 +0200

    T3 neu implementierung

---
+++ Assets/T3/T3Laser.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class T3Laser : MonoBehaviour {
+    
+    public int timeoffset;
+    public float power;
+
+    private ParticleSystem sys;
+    private System.DateTime start;
+    private System.DateTime end;
+    private Vector3 pos;
+    private Vector3 dir;
+	// Use this for initialization
+	void Start () {
+        start = System.DateTime.Now;
+        sys = this.GetComponent<ParticleSystem>();
+        pos = sys.gameObject.transform.position;
+        dir = sys.gameObject.transform.forward;
+        if (timeoffset == 0) timeoffset = 5;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    end = System.DateTime.Now;
+        if (end.Subtract(start).Seconds > timeoffset)
+        {
+            start = System.DateTime.Now;
+
+            
+            RaycastHit hitinfo;
+            Physics.Raycast(new Ray(pos, dir), out hitinfo, sys.startLifetime*sys.startSpeed);
+
+            Collider col = hitinfo.collider;
+            Rigidbody rb;
+
+            sys.Play();
+            if (col == null) return;
+            rb = col.GetComponent<Rigidbody>();
+            if (rb == null && col.gameObject.transform.parent != null) rb = col.gameObject.transform.parent.GetComponent<Rigidbody>();
+
+            if (rb != null && !col.gameObject.name.Contains("Asteroid"))
+            {
+                try
+                {
+                    rb.AddExplosionForce(power, pos, sys.startLifetime * sys.startSpeed);
+                }
+                catch (MissingComponentException e)
+                {
+                }
+            }
+        }
+	}
+}
---
+++ Assets/T3/T3Schockwave.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class T3Schockwave : MonoBehaviour {
+
+
+    public int timeoffset;
+    public float r;
+    public float power;
+    private Vector3 pos;
+    public bool randomized;
+
+    private System.DateTime start;
+    private System.DateTime end;
+    // Use this for initialization
+    void Start()
+    {
+        pos = transform.position;
+        start = System.DateTime.Now;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        end = System.DateTime.Now;
+        if (end.Subtract(start).Seconds > 1) this.GetComponent<ParticleSystem>().enableEmission = false;
+        if (end.Subtract(start).Seconds > timeoffset)
+        {
+            int mod = (randomized) ? new System.Random().Next(0, 2) : 1;
+            Collider[] colliders = Physics.OverlapSphere(pos, r);
+            foreach (Collider hit in colliders)
+            {
+                Rigidbody rb;
+
+                rb = hit.GetComponent<Rigidbody>();
+                if (rb == null && hit.gameObject.transform.parent != null) rb = hit.gameObject.transform.parent.GetComponent<Rigidbody>();
+
+                if (rb != null && !hit.gameObject.name.Contains("Asteroid"))
+                {
+                    try
+                    {
+                        if (mod != 0)
+                        {
+                            rb.AddExplosionForce(power, pos, r, 0, ForceMode.Impulse);
+                            this.GetComponent<ParticleSystem>().enableEmission = true;
+                        }
+                    }
+                    catch (MissingComponentException e)
+                    {
+                        continue;
+                    }
+                }
+            }
+            start = System.DateTime.Now;
+        }
+    }
+}
