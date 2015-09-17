98f6f6f2c2cb828f043c8e3949f6473089e2f684
Tobi <ToZeimetz@aol.com>
2015-09-17 15:49:20 +0200

    T3 import

---
+++ Assets/T3/VEngineDriverT3.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VEngineDriverT3 : MonoBehaviour {
+
+	public int sign = 0;
+    Transform t;
+    void start()
+    {
+        t = this.GetComponent<Transform>();
+    }
+
+    //protected override float Filter(float f)
+    //{
+    //    return sign == (int)Mathf.Sign(f) ? Mathf.Abs(f) : 0f;
+    //}
+
+
+    //protected override string FetchAxis(Controller ctrl)
+    //{
+    //    return ctrl.verticalAxis;
+    //}
+
+    void FixedUpdate()
+    {
+        t.Rotate(new Vector3(0,90,0));
+    }
+}
