7f74648feb2a2f887b83925adb2cf8b3a5896469
kreutzch <kreutzch@uni-trier.de>
2015-09-20 01:36:53 +0200

    Alles EZ, halb 2. Locker 3 Tage wach. Yoliver, das ist fuer dich. Gute Luft. Credits: Voudi, Sera.

---
+++ Assets/T7/T7VEDrive.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class T7VEDrive : DirectEngineDriver {
+
+		public int sign = 0;
+		public float forceP = 0f;
+		
+		protected override float Filter(float f)
+		{
+			forceP = sign == (int)Mathf.Sign(f) ? Mathf.Abs(f) : 0f;
+			return forceP;
+		}
+		
+		protected override string FetchAxis(Controller ctrl)
+		{
+//			Debug.Log (ctrl.ctrlAxisVertical);
+			return ctrl.ctrlAxisVertical;
+		}
+}
---
+++ Assets/T7/T7ZielRingCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class T7ZielRingCollider : MonoBehaviour
+{
+    public int ringNr;
+    public T7Level levelLogic;
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.transform.root.name.StartsWith("Team"))
+        {
+            levelLogic.ringVisited(ringNr, col.gameObject.transform.root.gameObject);
+        }
+    }
+}
